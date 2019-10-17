087168e169bae876a2126ea7ad744e5be6914cd4
huguiyun <2322884305@qq.com>
2019-10-17 19:13:17 +0800

    Initial commit

---
+++ git/src/main/java/com/hu/text.java
@@ -0,0 +1,13 @@
+package com.hu;
+
+/**
+ * @author hu
+ * @site www.huguiyun.xzy
+ * @company xxx公司
+ * @create  2019-10-17 11:48
+ */
+public class text {
+    public static void main(String[] args) {
+        System.out.println("我是你爸爸，我真伟大！！！");
+    }
+}
